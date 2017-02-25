244d78cc3e71746c8abbbeee4bbfca39b64e8111
Danny Suarez <metasaval@gmail.com>
2017-02-25 12:06:43 -0500

    Now finds biggest tone and sends it to Arduino

--- LyricstoMood.py
+++ LyricstoMood.py
@@ -1,11 +1,18 @@
-import requests
+""" Code that uses IBM Watson to convert text to mood """
 
+import requests
+import time
+import serial
 
 # Supposed to return the lyrics to the song name passed in as a parameter
-# Needs to return a concatnated string of lyrics, search is not too efficient, needs to be made efficient
+# Needs to return a concatnated string of lyrics, search is not too efficient,
+# needs to be made efficient
 def lyrics(search):
-    url = "https://api.cognitive.microsoft.com/bing/v5.0/news/search?q=" + search + "&count=10&offset=0&mkt=en-us&safeSearch=Moderate"
-    header = {'Host': 'api.cognitive.microsoft.com', 'Ocp-Apim-Subscription-Key': 'a7c37c0e2b124cfe8e74f4824693d0f6'}
+    url = "https://api.cognitive.microsoft.com/bing/v5.0/news/search?q=" \
+    + search + "&count=10&offset=0&mkt=en-us&safeSearch=Moderate"
+
+    header = {'Host': 'api.cognitive.microsoft.com', \
+    'Ocp-Apim-Subscription-Key': 'a7c37c0e2b124cfe8e74f4824693d0f6'}
 
     resp = requests.get(url, headers=header)
     if resp.status_code != 200:
@@ -16,10 +23,10 @@ def lyrics(search):
     for a in range(0,len(respVal)):
         print(respVal[a]['description'])
 
-
 # Supposed to return the percentages of mood found in a text
 def moods(text):
-    url = 'https://watson-api-explorer.mybluemix.net/tone-analyzer/api/v3/tone?version=2016-05-19'
+    url = 'https://watson-api-explorer.mybluemix.net/tone-ana \
+    lyzer/api/v3/tone?version=2016-05-19'
     data = text
     username = '7144acc5-8b6b-4721-98c0-7696a547a450'
     password = 'W0LzbWyPDmsl'
@@ -34,8 +41,27 @@ def moods(text):
     respTones = respToneCat[0]
     respInTones = respTones['tones']
 
+    mostTone
+    mostPer = 0
+    currTone
+    currPer = 0
+
+    # iterates through the moods and returns the one with the highest percentage
     for a in range(0, len(respInTones)):
+        currPer = respIntones[a]['score']
+        if currPer > mostPer
+            mostPer = currPer
+            mostTone = respInTones[a]['tone_name']
         print("Score" + str(respInTones[a]['score']) + " " + str(respInTones[a]['tone_name']))
+    
+    # opening a serial port to send data to the Arduino
+    ser = serial.Serial('/dev/ttyUSB0', 9600)
+    time.sleep(2)
+
+    # write to serial
+    ser.write(mostTone[0])
+
+    ser.close()
 
     # Big thanks to Matt
 
