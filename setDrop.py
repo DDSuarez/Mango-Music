8fe3ce08f27c47fe43f23f472cf1b312288cb593
rubecube <rubcube2010@gmail.com>
2017-02-26 02:56:33 -0500

    Update setDrop.py

--- setDrop.py
+++ setDrop.py
@@ -8,7 +8,21 @@ file = open('/home/pi/Desktop/data.txt','r')
 lineList = file.readlines()
 file.close()
 cleanfile = str(lineList[-1:])
-simple = str(cleanfile[2:len(cleanfile)-4])
-split = simple.split('by ')
+split = [] 
+for x in range(5):
+  split.append(' ')
+#square = []
+#for x in range(5):
+#  square.append(x**2)
+#print(square)
+
+try:
+ simple = str(cleanfile[2:len(cleanfile)-4]) 
+ split = simple.split('by ')
+ split[0]
+ split[1]
+except Exception as e:
+ split[0]='Never gonna give you up'
+ split.append('Rick Astly')
 print(split[0])
 print(split[1])
