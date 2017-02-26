332cdd9ccb2258e6e915307acf177d44686c8e6f
Danny Suarez <metasaval@gmail.com>
2017-02-26 01:29:53 -0500

    Twitter database

--- LyricstoMood.py
+++ LyricstoMood.py
@@ -6,13 +6,24 @@ import serial
 import time
 from PyLyrics import *
 
-def twitterGrab():
+def twitterGrab(search):
     username = "c9fd668e-4e8e-4a9f-97ff-524b3282f8a9"
     password = "VTw4ScwxgI"
     host = "cdeservice.mybluemix.net"
     port = 443
-    url = "https://c9fd668e-4e8e-4a9f-97ff-524b3282f8a9:VTw4ScwxgI@cdeservice.mybluemix.net"
-    header = {"content-type": "text/plain"}
+    url = "https://cdeservice.mybluemix.net:443/api/v1/messages/search?1=" + search
+    header = {"Host":"cdeservice.mybluemix.net", "Port": 443, "Authorization": "Basic YzlmZDY2IGYtBGU4ZS00YTlmLTk3ZmYtNtI0YjMyODJmOGE501ZUdzRTY3d4Z0k="}
+    resp = requests.get(url, auth=(username, password), headers=header)
+
+    print(resp)
+
+def twitterDatabase()
+    username = "235c39c8-714b-440d-9415-57876cf9d02-bluemix"
+    password = "44629e5f840cd905917778926e8846b37658a3c4bd626aaac6f6f706d0acce27"
+    host = "235c39c8-714b-440d-941557876c2f9d02-bluemix.cloudant.com"
+    port = 443
+    url = "https://cdeservice.mybluemix.net:443/api/v1/messages/search?1=" + search
+    header = {"Host":"cdeservice.mybluemix.net", "Port": 443, "Authorization": "Basic YzlmZDY2IGYtBGU4ZS00YTlmLTk3ZmYtNtI0YjMyODJmOGE501ZUdzRTY3d4Z0k="}
     resp = requests.get(url, auth=(username, password), headers=header)
 
 # Returns the percentages of mood found in a text
@@ -52,8 +63,9 @@ def moods(text):
 artist = "Dance Gavin Dance"
 song = "Uneasy Hearts Weigh the Most"
 
-print(PyLyrics.getLyrics(artist, song))
-moods(PyLyrics.getLyrics(artist, song))
+#print(PyLyrics.getLyrics(artist, song))
+#moods(PyLyrics.getLyrics(artist, song))
+twitterGrab()
 
 # Supposed to return the lyrics to the song name passed in as a parameter
 # Needs to return a concatnated string of lyrics, search is not too efficient,
